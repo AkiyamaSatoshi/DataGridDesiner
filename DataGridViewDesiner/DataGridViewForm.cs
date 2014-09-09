e149ed92e53f61bff1fc7d22a9d42416c492a9c1
Akiyan <akiyans@gmail.com>
2014-09-10 01:55:57 +0900

    FIrst Commit

---
+++ DataGridViewDesiner/DataGridViewForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridViewDesiner
+{
+    public partial class DataGridViewForm : Form
+    {
+        public DataGridViewForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGridViewForm_Load(object sender, EventArgs e)
+        {
+            // TODO: このコード行はデータを 'tESTRDBDataSet.MU_MATERIAL' テーブルに読み込みます。必要に応じて移動、または削除をしてください。
+            this.mU_MATERIALTableAdapter.Fill(this.tESTRDBDataSet.MU_MATERIAL);
+
+        }
+    }
+}
